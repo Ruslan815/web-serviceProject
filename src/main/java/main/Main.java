d6a435dce826778d99fe591fbc702c2c5b5044fc
Ruslan815 <ruslansaf2000@gmail.com>
2021-06-27 17:14:02 +0700

    Created example of working with database

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,39 +1,23 @@
 package main;
 
-import accounts.AccountService;
-import accounts.UserProfile;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.SessionsServlet;
-import servlets.UsersServlet;
+import dbService.DBException;
+import dbService.DBService;
+import dbService.dataSets.UsersDataSet;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        AccountService accountService = new AccountService();
+    public static void main(String[] args) {
+        DBService dbService = new DBService();
+        dbService.printConnectInfo();
+        try {
+            long userId = dbService.addUser("tully");
+            System.out.println("Added user id: " + userId);
 
-        accountService.addNewUser(new UserProfile("admin"));
-        accountService.addNewUser(new UserProfile("test"));
+            UsersDataSet dataSet = dbService.getUser(userId);
+            System.out.println("User data set: " + dataSet);
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-
-        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/signin");
-        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/signup");
-
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setResourceBase("public_html");
-
-        HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{resource_handler, context}); // Важен порядок, чем раньше указан - тем выше приоритет исполнения (первее исполнится)
-
-        Server server = new Server(8080);
-        server.setHandler(handlers);
-
-        server.start();
-        System.out.println("Server started");
-        server.join();
+            dbService.cleanUp();
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
     }
 }
